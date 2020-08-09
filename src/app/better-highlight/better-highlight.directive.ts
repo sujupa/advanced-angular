7544dc5fbb2cbe7649456c5ab5c2f60eb279c3e4
Sujay Patil <sujay.patil@BRILLIO.COM>
2020-08-09 22:47:50 +0530

    added HostListeners and HostBindings

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,11 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
 
-  constructor() { }
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  constructor(private renderer: Renderer2,
+    private elRef: ElementRef) { }
+
+  ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+  }
 
 }
