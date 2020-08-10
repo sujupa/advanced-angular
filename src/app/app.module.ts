dc2d7b3302224d85b6d2f9638fc108e8b4416ffd
Sujay Patil <sujay.patil@BRILLIO.COM>
2020-08-10 20:41:28 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 import { UnlessDirective } from './unless.directive';
+import { TrialComponent } from './trial/trial.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'trial', component: TrialComponent }
+];
 
 @NgModule({
   declarations: [
@@ -13,10 +21,12 @@ import { UnlessDirective } from './unless.directive';
     TestComponent,
     BasicHighlightDirective,
     BetterHighlightDirective,
-    UnlessDirective
+    UnlessDirective,
+    TrialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
