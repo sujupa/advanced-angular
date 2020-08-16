7cfed1adfd630b581fcce36924c2cf11ffe600ce
Sujay Patil <sujay.patil@BRILLIO.COM>
2020-08-16 14:49:52 +0530

    observables and updated notes

--- src/app/trial/trial.component.ts
+++ src/app/trial/trial.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { interval, Subscription, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-trial',
   templateUrl: './trial.component.html',
@@ -10,6 +13,9 @@ export class TrialComponent implements OnInit, OnDestroy {
 
   idFromURL: string = "";
   nameFromURL: string = "";
+
+  private firstObsSubscription: Subscription;
+  private customIntervalObservable: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -32,10 +38,63 @@ export class TrialComponent implements OnInit, OnDestroy {
       this.nameFromURL = params['name'];
     });
 
+    // Observables
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log("Count: ", count);
+
+    // });
+
+    // Custom Observables
+    const customIntervalObservable1234 = Observable.create((observer) => {
+
+      let count = 0;
+
+      setInterval(() => {
+
+        observer.next(count);
+
+        if (count === 2) {
+          observer.complete();
+        }
+
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3!!"));
+        }
+        count++;
+
+      }, 1000);
+
+    });
+
+    // Only map function
+    // const pipeValue = customIntervalObservable1234.pipe(map((data: number) => {
+    //   return 'Round: ' + (data + 1);
+    // }));
+
+    // Both map and filter
+    const pipeValue = customIntervalObservable1234.pipe(filter((data: number) => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    }));
+
+    this.customIntervalObservable = pipeValue.subscribe(data => {
+      console.log("Custom Obervable Data: ", data);
+    }, error => {
+      console.log("Error got in subscribe: ", error);
+    }, () => {
+      console.log('Completed!');
+    }
+
+    );
+
+
+
   }
 
   ngOnDestroy() {
-
+    // this.firstObsSubscription.unsubscribe();
+    this.customIntervalObservable.unsubscribe();
   }
 
 }
